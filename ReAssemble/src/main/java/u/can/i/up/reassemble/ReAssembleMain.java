b6b2997a1a5784c1365a625a85430e400935add3
lczgywzyy <xyq547133@163.com>
2015-11-23 16:11:22 +0800

    新建反编译模块

---
+++ ReAssemble/src/main/java/u/can/i/up/reassemble/ReAssembleMain.java
@@ -0,0 +1,15 @@
+package u.can.i.up.reassemble;
+
+import java.io.IOException;
+
+import brut.apktool.Main;
+import brut.common.BrutException;
+
+/**
+ * Created by lczgywzyy on 2015/11/23.
+ */
+public class ReAssembleMain {
+    public static void main(String[] args) throws IOException, InterruptedException, BrutException {
+        Main.main(args);
+    }
+}
